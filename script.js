dbe000412a2b7897dd6fdcec9dd7adf33de4ad8b
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-07 22:19:19 +0530

    code modified

--- script.js
+++ script.js
@@ -8,7 +8,43 @@ const getMovieInfo = async (movie) => {
 
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+  //   console.log(data);
+
+  showMovieData(data);
+};
+
+const showMovieData = (data) => {
+  movieContainer.innerHTML = "";
+  const { Title, imdbRating, Genre, Released, Runtime, Actors, Plot, Poster } =
+    data;
+
+  const movieElement = document.createElement("div");
+  //   movieGenreElement.classList.add("movie-info");
+  movieElement.innerHTML = `<h2>${Title}</h2>
+  <p><strong>Rating: &#11088</strong>${imdbRating}</p>`;
+
+  const movieGenreElement = document.createElement("div");
+  movieGenreElement.classList.add("movie-genre");
+
+  Genre.split(",").forEach((element) => {
+    const p = document.createElement("p");
+    p.innerText = element;
+    movieGenreElement.appendChild(p);
+  });
+
+  movieElement.appendChild(movieGenreElement);
+
+  movieElement.innerHTML += `<p><strong>Released Date: </strong>${Released}</p>
+  <p><strong>Duration: </strong>${Runtime}</p>
+  <p><strong>Cast: </strong>${Actors}</p>
+  <p><strong>Plot: </strong>${Plot}</p>`;
+
+  const moviePosterElement = document.createElement("div");
+  moviePosterElement.classList.add("movie-poster");
+  moviePosterElement.innerHTML = `<img src="${Poster}"/>`;
+
+  movieContainer.appendChild(moviePosterElement);
+  movieContainer.appendChild(movieElement);
 };
 
 searchForm.addEventListener("submit", (event) => {
